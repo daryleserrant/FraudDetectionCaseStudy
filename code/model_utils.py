51fb487757f47007d652fa1f819f5d2dadb0e2ca
gSchool Student <gSchool@Datas-Mac-mini-48.local>
2016-06-15 16:59:17 -0700

    Initial Project Commit

---
+++ code/model_utils.py
@@ -0,0 +1,7 @@
+from sklearn import cross_validation
+from sklearn.linear_model import LogisticRegression
+from feature_utils import featurize_data
+
+
+def train_logistic_regression_model(jsonfile):
+    pass
