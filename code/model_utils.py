8c06469e557d0490601588762cd491dcb61ae03d
Catherine L <catherinelee@MacBook-Pro-3.local>
2016-06-16 11:59:00 -0700

    need to pickle

--- code/model_utils.py
+++ code/model_utils.py
@@ -1,17 +1,30 @@
 from sklearn import cross_validation
-from feature_utils import featurize_data
+from feature_utils import featurize_data, oversample
 import pandas as pd
 import numpy as np
 import json
 import time
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegressionCV
+from sklearn.metrics import recall_score
+
+class Fraud(object):
+
+	def __init__(self):
+		self.model = LogisticRegressionCV(cv=15, scoring = recall_score)
+		self.fitted = None
 
 
-def train_logistic_regression_model(jsonfile):
-	#Featurize first and split data from library
+	def fit(self, jsonfile, target=0.3):
+		X, y = featurize_data(jsonfile)
 
-	lr = LogisticRegression()
-	lr.fit(X_train, y_train)
-	lr.score(X_test, y_test)
-    return 
+		#Balance the classes
+		X_oversample, y_oversample = oversample(X, y, target) 
+
+		#Fit the model
+	    self.fitted = self.model.fit(X_oversample, y_oversample)
+
+	 def predict(self, X_test):
+	 	return self.fitted.predict_proba(X_test)[:, 1]
+
+	 	
